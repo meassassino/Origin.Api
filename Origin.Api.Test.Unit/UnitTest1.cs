562b18e61a6eca004562c0a779d5a748395b00a5
Purav Upadhyay <Purav.Upadhyay@mmtdigital.co.uk>
2024-06-13 15:21:22 +0100

    project re-structure
    had to re-structure the project to allow adding of BDD and Unit test projects.

---
+++ Origin.Api.Test.Unit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Origin.Api.Test.Unit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
